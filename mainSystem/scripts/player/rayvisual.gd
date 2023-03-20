6dc9a49528d83a8ee84a7e028cf938b91426ddd3
zodiepupper <zodywoolsey@users.noreply.github.com>
2023-03-20 01:23:11 -0600

    upd

--- mainSystem/scripts/player/rayvisual.gd
+++ mainSystem/scripts/player/rayvisual.gd
@@ -1,16 +1,17 @@
 extends RayCast3D
 
-var vis := MeshInstance3D.new()
-var vismat := StandardMaterial3D.new()
-
+var rayvis = preload("res://mainSystem/scenes/player/rayvis.tscn")
+var vis
 func _ready():
-	vismat.no_depth_test = true
-	vis.mesh = BoxMesh.new()
-	vis.mesh.size = Vector3(.01,.01,.01)
-	vis.mesh.surface_set_material(0,vismat)
+	vis = rayvis.instantiate()
 	get_tree().root.add_child.call_deferred(vis)
+	
 
 func _process(delta):
+	if !is_colliding():
+		vis.hide()
+	elif !vis.visible:
+		vis.show()
 	var tmp = get_collision_point()
 	vis.global_position.x = lerpf(vis.global_position.x, tmp.x, .1)
 	vis.global_position.z = lerpf(vis.global_position.z, tmp.z, .1)
