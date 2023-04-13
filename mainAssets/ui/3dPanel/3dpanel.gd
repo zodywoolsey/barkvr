6561b9f348a7f1309628d0a356b40f8e5020ed40
zodiepupper <zodywoolsey@users.noreply.github.com>
2023-04-13 14:41:16 -0600

    upd

--- mainAssets/ui/3dPanel/3dpanel.gd
+++ mainAssets/ui/3dPanel/3dpanel.gd
@@ -2,9 +2,13 @@ extends Node3D
 @export var viewport : SubViewport
 @export var mesh : MeshInstance3D
 @export var colShape : CollisionShape3D
+@onready var static_body_3d = $panel/StaticBody3D
 
 func _ready():
 	mesh.mesh.material.albedo_texture = viewport.get_texture()
 
 func _process(delta):
 	colShape.shape.size = Vector3(mesh.mesh.size.x,.01,mesh.mesh.size.y)
+
+func laserClick(pos):
+	print(pos)
