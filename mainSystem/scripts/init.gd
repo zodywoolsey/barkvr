2fbc0b4345f399020680465994b6b688949502ab
zodiepupper <zodywoolsey@users.noreply.github.com>
2023-08-20 18:59:56 -0600

    remove shitty avatar test

--- mainSystem/scripts/init.gd
+++ mainSystem/scripts/init.gd
@@ -6,6 +6,19 @@ func _ready():
 	start_xr.xr_started.connect(func():
 		LocalGlobals.vr_supported = true
 		)
+	get_window().files_dropped.connect(func(files):
+		print("files: \n\n",files)
+		var object_file = FileAccess.open(files[0], FileAccess.READ_WRITE)
+		if object_file:
+			var tmp = object_file.get_as_text()
+			var loaded_object = BarkHelpers.var_to_node(tmp)
+			var localworld = get_tree().get_first_node_in_group("localworldroot")
+			if localworld:
+				print("loaded object: ",str(loaded_object))
+				var parent = get_tree().get_first_node_in_group('localworldroot')
+				if parent:
+					parent.add_child(loaded_object)
+		)
 #	Vector.user_logged_in.connect(func():
 #		discord_sdk.details = "local home, logged in"
 #		discord_sdk.refresh()
