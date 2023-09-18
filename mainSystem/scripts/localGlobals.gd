239ec7691220b4f123d8ecb131fc71cc703d9512
zodiepupper <zodywoolsey@users.noreply.github.com>
2023-09-17 18:14:02 -0600

    upd, fixed webxr kinda (still very buggy)

--- mainSystem/scripts/localGlobals.gd
+++ mainSystem/scripts/localGlobals.gd
@@ -32,18 +32,18 @@ func _ready():
 		OS.request_permissions()
 
 	# Application ID
-	discord_sdk.app_id = 1137953671842377778
-	# this is boolean if everything worked
-	print("Discord working: " + str(discord_sdk.get_is_discord_working()))
-	print(discord_sdk.get_current_user())
-	discord_sdk.details = discord_world
-	discord_sdk.state = discord_login_status
-	discord_sdk.large_image = "game"
-	discord_sdk.start_timestamp = int(Time.get_unix_time_from_system())
-	get_tree().create_timer(1).timeout.connect(_update_discord)
+# 	discord_sdk.app_id = 1137953671842377778
+# 	# this is boolean if everything worked
+# 	print("Discord working: " + str(discord_sdk.get_is_discord_working()))
+# 	print(discord_sdk.get_current_user())
+# 	discord_sdk.details = discord_world
+# 	discord_sdk.state = discord_login_status
+# 	discord_sdk.large_image = "game"
+# 	discord_sdk.start_timestamp = int(Time.get_unix_time_from_system())
+# 	get_tree().create_timer(1).timeout.connect(_update_discord)
 
-func _update_discord():
-	discord_sdk.details = discord_world
-	discord_sdk.state = discord_login_status
-	discord_sdk.refresh() 
-	get_tree().create_timer(1).timeout.connect(_update_discord)
+# func _update_discord():
+# 	discord_sdk.details = discord_world
+# 	discord_sdk.state = discord_login_status
+# 	discord_sdk.refresh() 
+# 	get_tree().create_timer(1).timeout.connect(_update_discord)
