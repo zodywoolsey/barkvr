2a20a9943ee971d0a3a77a57094cd79a30309107
zodiepupper <zodywoolsey@users.noreply.github.com>
2023-09-19 16:45:15 -0600

    voip in progress

--- mainSystem/scripts/mic_playback.gd
+++ mainSystem/scripts/mic_playback.gd
@@ -11,11 +11,11 @@ func _process(delta):
 	var phase = 0.0
 	var increment = 150.0 / sample_hz
 	print(playback_stream.get_frames_available())
-	for i in range(10000):
+	for i in range(playback_stream.get_frames_available()):
 		if buffer_to_push.size()>0:
 			playback_stream.push_frame(buffer_to_push.pop_back())
 		else:
-			playback_stream.push_frame(Vector2.ONE * sin(phase * TAU))
+			playback_stream.push_frame(Vector2.ONE * sin(phase * TAU)/8.0)
 			phase = fmod(phase + increment, 1.0)
 
 func set_buffer(data:PackedVector2Array):
