ca219d607ce17db1030b19e96bc6377eb9a7d3b6
zodiepupper <zodie@pupper.dev>
2026-09-12 15:21:56 -0400

    fix double resizing for panels when using the resizer

--- barkvr-system/ui/3dPanel/panel_resizer.gd
+++ barkvr-system/ui/3dPanel/panel_resizer.gd
@@ -6,7 +6,5 @@ func _ready() -> void:
 
 func _input(event: InputEvent) -> void:
 	if button_pressed and event is InputEventMouseMotion and get_viewport().get_parent() is Panel3D:
-		#get_viewport().size.y = event.position.y as int
-		#get_viewport().size.x = event.position.x as int
 		if get_viewport().get_parent() is Panel3D:
 			get_viewport().get_parent().viewport_size = Vector2i(event.position.x as int, event.position.y as int)
